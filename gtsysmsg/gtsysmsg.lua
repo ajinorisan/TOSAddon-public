0e76f415b1d177c956ff0ce472a560cc2bf5a2cc
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-04-08 09:53:13 +0900

    Add files via upload

--- gtsysmsg/gtsysmsg.lua
+++ gtsysmsg/gtsysmsg.lua
@@ -1,6 +1,7 @@
 local addonName = "GTSYSMSG"
 local addonNameLower = string.lower(addonName)
 local author = "norisan"
+local ver = "1.0.5"
 
 _G["ADDONS"] = _G["ADDONS"] or {}
 _G["ADDONS"][author] = _G["ADDONS"][author] or {}
@@ -11,6 +12,8 @@ g.settingsDirLoc = string.format("../addons/%s", addonNameLower)
 g.settingsFileLoc = string.format("%s/settings.json", g.settingsDirLoc)
 
 local acutil = require("acutil")
+
+  --imcSound.PlaySoundEvent('sys_tp_box_3',nil,true);
 
 function GTSYSMSG_ON_INIT(addon, frame)
     
@@ -26,19 +29,28 @@ function GTSYSMSG_ON_INIT(addon, frame)
     acutil.setupHook(GTSYSMSG_ON_MSG, "NOTICE_ON_MSG")
     addon:RegisterMsg('GAME_START_3SEC', 'GTSYSMSG_ON_MSG')
     CHAT_SYSTEM("GTSYSMSG_ON_INIT")
-end
+end   
 
 function GTSYSMSG_ON_MSG(frame, msg, argStr, argNum)
-    GTSYSMSG_ALL(frame, msg, argStr, argNum)
+    if argStr ~= "None" then
+        CHAT_SYSTEM(argStr)
+        if string.find(argStr, 'AppearPCMonster{name}') then
+            GTSYSMSG_CENTER_MSG(argStr)
+            imcSound.PlaySoundEvent('sys_tp_box_4')
+        end
+
+        if string.find(argStr, '{name}DisappearPCMonster') then
+            GTSYSMSG_CENTER_MSG(argStr)
+            --imcSound.PlaySoundEvent('sys_tp_box_3')
+        end
+
+        if string.find(argStr, 'AppearFieldBoss_ep14_2_d_castle_3{name}') then
+            GTSYSMSG_CENTER_MSG(argStr)
+            --imcSound.PlaySoundEvent('sys_tp_box_3')
+        end
+    end
 end
 
 function GTSYSMSG_CENTER_MSG(argStr)
     NICO_CHAT(string.format("{@st55_a}%s", argStr))
-end
-
-function GTSYSMSG_ALL(frame, msg, argStr, argNum)
-if argStr ~= "None"  then　--マップ読み込むたびにNoneって出るので直してみました。
-    CHAT_SYSTEM(argStr)
-    GTSYSMSG_CENTER_MSG(argStr)
-    end
 end
