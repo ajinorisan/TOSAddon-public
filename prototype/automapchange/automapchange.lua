eb8568759a50f141ac786c16dcee3c93ccea9391
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-05-09 08:11:59 +0900

    a

--- prototype/automapchange/automapchange.lua
+++ prototype/automapchange/automapchange.lua
@@ -8,13 +8,24 @@ _G["ADDONS"][author] = _G["ADDONS"][author] or {}
 _G["ADDONS"][author][addonName] = _G["ADDONS"][author][addonName] or {}
 local g = _G["ADDONS"][author][addonName]
 
-g.settingsFileLoc = string.format("%s/settings.json", addonNameLower)
+g.settingsDirLoc = string.format("../addons/%s", addonNameLower)
+g.settingsFileLoc = string.format("../addons/%s/settings.json", addonNameLower);
 
 local acutil = require("acutil")
 
---true=on false=off
-if not g.loaded then
-g.settings = {onoff = true}
+function AUTOMAPCHANGE_ON_INIT(addon, frame)
+    g.addon = addon
+    g.frame = frame
+    
+    --frame:ShowWindow(0);
+  
+    addon:RegisterMsg("DIALOG_CHANGE_SELECT", "AUTOMAPCHANGE_DIALOG_ON_MSG")
+    addon:RegisterMsg('GAME_START', 'AUTOMAPCHANGE_LOADSETTINGS')
+    acutil.slashCommand("/amc",AUTOMAPCHANGE_CMD)
+    acutil.slashCommand("/automapchange",AUTOMAPCHANGE_CMD)
+    CHAT_SYSTEM("automapchange loaded")
+    camera.CustomZoom(700)
+   
 end
 
 function AUTOMAPCHANGE_SAVESETTINGS()
@@ -22,40 +33,23 @@ function AUTOMAPCHANGE_SAVESETTINGS()
 end
 
 function AUTOMAPCHANGE_LOADSETTINGS()
-	acutil.loadJSON(g.settingsFileLoc, g.settings);
-end
-
-function AUTOMAPCHANGE_ON_INIT(addon, frame)
-    g.addon = addon
-    g.frame = frame
-    
-    frame:ShowWindow(0);
-    
-    if not g.loaded then
+	
+	if g.settings == nil then
+	 g.settings = {onoff = true}
+	 AUTOMAPCHANGE_SAVESETTINGS()
+	end
+	
     local t, err = acutil.loadJSON(g.settingsFileLoc, g.settings);
-    if err then
+     if err then
       --設定ファイル読み込み失敗時処理
       CHAT_SYSTEM(string.format("[%s] cannot load setting files", addonName))
-    else
+     else
       --設定ファイル読み込み成功時処理
       g.settings = t;
-    end
+     end
     g.loaded = true
-    end
-    
-    AUTOMAPCHANGE_LOADSETTINGS()
-    --AUTOMAPCHANGE_SAVESETTINGS();
-  
-    addon:RegisterMsg("DIALOG_CHANGE_SELECT", "AUTOMAPCHANGE_DIALOG_ON_MSG")
-    addon:RegisterMsg('GAME_START_3SEC', 'AUTOMAPCHANGE_DIALOG_ON_MSG')
-    acutil.slashCommand("/amc",AUTOMAPCHANGE_CMD)
-    acutil.slashCommand("/automapchange",AUTOMAPCHANGE_CMD)
-    --ReserveScript(string.format("/amc"),0.5)
-    --ReserveScript(string.format("/amc"),0.5)
-    CHAT_SYSTEM("automapchange loaded")
-    camera.CustomZoom(700)
-   
 end
+
 
 function AUTOMAPCHANGE_CMD(command)
     if g.settings.onoff == true then
@@ -71,9 +65,9 @@ function AUTOMAPCHANGE_CMD(command)
 end
 
 function AUTOMAPCHANGE_DIALOG_ON_MSG(frame, msg, argStr, argNum)
-print(msg)
-print(argStr)
-print(argNum)
+--print(msg)
+--print(argStr)
+--print(argNum)
     if g.settings.onoff == true then
         if string.find(argStr, "HighLvZoneEnterMsgCustom") ~= nil then
              local dialog = ui.GetFrame("dialog")
@@ -88,8 +82,8 @@ end
 function AUTOMAPCHANGE_DIALOGSELECT()
     control.DialogSelect(1)
     local x, y = mouse.GetX(), mouse.GetY()
-    string.format('mouse.SetPos(%d, %d)', x, y)
-end
+     ReserveScript(string.format('mouse.SetPos(%d, %d)', x, y),0.5)
+     end
 
 --[[
 function BHMAPCHANGE_DIALOGSELECT_ON_MSG(frame, msg, argStr, argNum)
