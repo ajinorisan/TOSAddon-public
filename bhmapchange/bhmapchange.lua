04648d2d5b97a16119af8781f8779a1b022a332c
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-05-08 14:22:46 +0900

    a

--- bhmapchange/bhmapchange.lua
+++ bhmapchange/bhmapchange.lua
@@ -13,35 +13,43 @@ g.settingsFileLoc = string.format("%s/settings.json", g.settingsDirLoc)
 
 local acutil = require("acutil")
 
-function BHMAPCHANGE_ON_INIT(addon, frame)
+local BHModeOn = false
 
+function BHMAPCHANGE_ON_INIT(addon, frame)
     g.addon = addon
     g.frame = frame
-    
-	addon:RegisterMsg("DIALOG_CHANGE_SELECT", "BHMAPCHANGE_DIALOG_ON_MSG")
-	addon:RegisterMsg("DIALOG_ADD_SELECT","BHMAPCHANGE_DIALOGSELECT_ON_MSG")
+
+    addon:RegisterMsg("DIALOG_CHANGE_SELECT", "BHMAPCHANGE_DIALOG_ON_MSG")
+    addon:RegisterMsg("DIALOG_ADD_SELECT", "BHMAPCHANGE_DIALOGSELECT_ON_MSG")
     CHAT_SYSTEM("BHMAPCHANGE loaded")
-    
+
+    local bhframe = ui.GetFrame("bountyhunt")
+    if bhframe:IsVisible() == 1 then
+        BHModeOn = true
+    else
+        BHModeOn = false
+    end
+    if BHModeOn then
+        camera.CustomZoom(700)
+    end
 end
 
 function BHMAPCHANGE_DIALOG_ON_MSG(frame, msg, argStr, argNum)
-
-    if string.find(argStr, "HighLvZoneEnterMsgCustom") ~= nil then
-   ReserveScript(ui.CloseFrame("dialog"), 0.01)
-     end
-    
+    if BHModeOn then
+        if string.find(argStr, "HighLvZoneEnterMsgCustom") ~= nil then
+            ReserveScript(ui.CloseFrame("dialog"), 0.01)
+        end
+    end
 end
 
 function BHMAPCHANGE_DIALOGSELECT()
-control.DialogSelect(1)
+    control.DialogSelect(1)
     local x, y = mouse.GetX(), mouse.GetY()
     ReserveScript(string.format('mouse.SetPos(%d, %d)', x, y), 0.02)
 end
 
-function NOCHECK_DIALOGSELECT_ON_MSG(frame, msg, argStr, argNum)
+function BHMAPCHANGE_DIALOGSELECT_ON_MSG(frame, msg, argStr, argNum)
     if argStr == '!@#$WS_ZACHA2F_01_TO_02_ANSWER_GO#@!' then
-       
-       NOCHECK_DIALOGSELECT()
+        BHMAPCHANGE_DIALOGSELECT()
     end
 end
-
