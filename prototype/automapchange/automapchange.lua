b24c1a11d0fb295ea7dc1711711861ee0e5cff92
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-05-10 14:26:51 +0900

    a

--- prototype/automapchange/automapchange.lua
+++ prototype/automapchange/automapchange.lua
@@ -14,28 +14,32 @@ g.settingsFileLoc = string.format("../addons/%s/settings.json", addonNameLower);
 local acutil = require("acutil")
 
 if not g.loaded then
- g.settings = {onoff = 1};
+    g.settings = {
+        onoff = 1
+    };
 end
 
 function AUTOMAPCHANGE_SAVESETTINGS()
-	acutil.saveJSON(g.settingsFileLoc, g.settings);
+    acutil.saveJSON(g.settingsFileLoc, g.settings);
 end
 
 function AUTOMAPCHANGE_LOADSETTINGS()
-	
-	if g.settings == nil then
-	 g.settings = {onoff = 1}
-	 AUTOMAPCHANGE_SAVESETTINGS()
-	end
-	
+
+    if g.settings == nil then
+        g.settings = {
+            onoff = 1
+        }
+        AUTOMAPCHANGE_SAVESETTINGS()
+    end
+
     local t, err = acutil.loadJSON(g.settingsFileLoc, g.settings);
-     if err then
-      --設定ファイル読み込み失敗時処理
-      CHAT_SYSTEM(string.format("[%s] cannot load setting files", addonName))
-     else
-      --設定ファイル読み込み成功時処理
-      g.settings = t;
-     end
+    if err then
+        -- 設定ファイル読み込み失敗時処理
+        CHAT_SYSTEM(string.format("[%s] cannot load setting files", addonName))
+    else
+        -- 設定ファイル読み込み成功時処理
+        g.settings = t;
+    end
     g.loaded = true
 end
 
@@ -45,13 +49,13 @@ function AUTOMAPCHANGE_CMD(command)
         g.settings.onoff = 0
         AUTOMAPCHANGE_SAVESETTINGS()
         CHAT_SYSTEM('AutoChangeMapOff')
-        
+
     else
-    
+
         g.settings.onoff = 1
         AUTOMAPCHANGE_SAVESETTINGS()
-       CHAT_SYSTEM('AutoChangeMapOn')
-       
+        CHAT_SYSTEM('AutoChangeMapOn')
+
     end
     return;
 end
@@ -60,36 +64,36 @@ function AUTOMAPCHANGE_ON_INIT(addon, frame)
 
     g.addon = addon
     g.frame = frame
-    
+
     addon:RegisterMsg("DIALOG_CHANGE_SELECT", "AUTOMAPCHANGE_DIALOG_ON_MSG")
     addon:RegisterMsg('GAME_START', 'AUTOMAPCHANGE_DIALOG_ON_MSG')
     addon:RegisterMsg('GAME_START', 'AUTOMAPCHANGE_LOADSETTINGS')
-    acutil.slashCommand("/amc",AUTOMAPCHANGE_CMD)
-    acutil.slashCommand("/automapchange",AUTOMAPCHANGE_CMD)
+    acutil.slashCommand("/amc", AUTOMAPCHANGE_CMD)
+    acutil.slashCommand("/automapchange", AUTOMAPCHANGE_CMD)
     CHAT_SYSTEM("automapchange loaded")
     camera.CustomZoom(700)
-    
+
 end
 
 function AUTOMAPCHANGE_DIALOG_ON_MSG(frame, msg, argStr, argNum)
 
     if g.settings.onoff == 1 then
         if string.find(argStr, "HighLvZoneEnterMsgCustom") ~= nil then
-       
-             local x, y = mouse:GetX(), mouse:GetY()
-             ReserveScript(string.format('mouse.SetPos(%d, %d)', x, y),0.1)
-             
+
+            local x, y = mouse:GetX(), mouse:GetY()
+            ReserveScript(string.format('mouse.SetPos(%d, %d)', x, y), 0.1)
+
             AUTOMAPCHANGE_DIALOGSELECT()
-            
+
         end
     end
-    
+
 end
 
 function AUTOMAPCHANGE_DIALOGSELECT()
- 
-         control.DialogSelect(1)
-         local x, y = mouse.GetX(), mouse.GetY()
-         ReserveScript(string.format('mouse.SetPos(%d, %d)', x, y),0.2)
- 
+
+    control.DialogSelect(1)
+    local x, y = mouse.GetX(), mouse.GetY()
+    ReserveScript(string.format('mouse.SetPos(%d, %d)', x, y), 0.2)
+
 end
