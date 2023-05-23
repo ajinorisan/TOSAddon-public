cd634d92099348163389d66f089e7767317cb23a
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-05-23 13:28:02 +0900

    a

--- guildeventwarp/guildeventwarp.lua
+++ guildeventwarp/guildeventwarp.lua
@@ -11,94 +11,111 @@ local g = _G["ADDONS"][author][addonName]
 local acutil = require("acutil")
 
 g.SettingsFileLoc = string.format('../addons/%s/settings.json', addonNameLower)
-
--- GUILDEVENTWARP_CH_CHANGE()
+g.ctrl = 0
 
 function GUILDEVENTWARP_ON_INIT(addon, frame)
     g.addon = addon
     g.frame = frame
+
+    local frame = ui.GetFrame('guildeventwarp')
+    frame:SetSkinName('None')
+    frame:Resize(95, 30)
+    frame:SetPos(1790, 5)
+    frame:SetTitleBarSkin("None")
+
+    GUILDEVENTWARP_FRAME_INIT(frame)
+
+    addon:RegisterMsg("GAME_START_3SEC", "GUILDEVENTWARP_ON_BORUTA_CLICK")
+    addon:RegisterMsg("GAME_START_3SEC", "GUILDEVENTWARP_ON_GILTINE_CLICK")
+    addon:RegisterMsg("GAME_START_3SEC", "GUILDEVENTWARP_ON_BAUBAS_CLICK")
+
     CHAT_SYSTEM(addonNameLower .. " loaded")
 
-    GUILDEVENTWARP_FRAME_INIT()
-    -- GUILDEVENTWARP_CH_CHANGE()
+    if g.ctrl == 1 then
+        GUILDEVENTWARP_ON_BORUTA_CLICK(frame)
+
+    elseif g.ctrl == 2 then
+        GUILDEVENTWARP_ON_GILTINE_CLICK(frame)
+
+    elseif g.ctrl == 3 then
+        GUILDEVENTWARP_ON_BAUBAS_CLICK(frame)
+
+    end
+
 end
 
-function GUILDEVENTWARP_FRAME_INIT()
+function GUILDEVENTWARP_FRAME_INIT(frame)
 
-    local gewframe = ui.GetFrame('guildeventwarp')
-    gewframe:SetSkinName('None')
-    gewframe:Resize(95, 30)
-    gewframe:SetPos(1790, 5)
-    gewframe:SetTitleBarSkin("None")
     -- ボルタボタン
-    local borutabutton = gewframe:CreateOrGetControl('button', 'boruta', 0, 0, 30, 30)
+    local borutabutton = frame:CreateOrGetControl('button', 'boruta', 0, 0, 30, 30)
     borutabutton:SetSkinName("test_red_button")
     borutabutton:SetText("{img emoticon_0007 29 29}")
     borutabutton:SetEventScript(ui.LBUTTONUP, "GUILDEVENTWARP_ON_BORUTA_CLICK")
 
     -- ギルティネボタン
-    local giltinebutton = gewframe:CreateOrGetControl('button', 'giltine', 35, 0, 30, 30)
+    local giltinebutton = frame:CreateOrGetControl('button', 'giltine', 35, 0, 30, 30)
     giltinebutton:SetSkinName("test_red_button")
     giltinebutton:SetText("Gi")
     giltinebutton:SetEventScript(ui.LBUTTONUP, "GUILDEVENTWARP_ON_GILTINE_CLICK")
 
     -- バウバスボタン
-    local baubasbutton = gewframe:CreateOrGetControl('button', 'baubas', 65, 0, 30, 30)
+    local baubasbutton = frame:CreateOrGetControl('button', 'baubas', 65, 0, 30, 30)
     baubasbutton:SetSkinName("test_red_button")
     baubasbutton:SetText("Ba")
     baubasbutton:SetEventScript(ui.LBUTTONUP, "GUILDEVENTWARP_ON_BAUBAS_CLICK")
-    --[[
-    -- バウバスボタン
-    local baubasbutton = gewframe:CreateOrGetControl('button', 'baubas', 65, 0, 30, 30)
-    baubasbutton:SetSkinName("test_red_button")
-    baubasbutton:SetText("Ba")
-    baubasbutton:SetEventScript(ui.LBUTTONUP, "GUILDEVENTWARP_ON_BAUBAS_CLICK")
-]]
-    gewframe:ShowWindow(1)
+
+    frame:ShowWindow(1)
 end
 
-function GUILDEVENTWARP_ON_BORUTA_CLICK(gewframe)
+function GUILDEVENTWARP_ON_BORUTA_CLICK(frame)
     -- CHAT_SYSTEM("ボルタボタンがクリックされました")
+
     local channelnum = session.loginInfo.GetChannel() + 1;
     if channelnum ~= 1 then
+        g.ctrl = 1
         GUILDEVENTWARP_CH_CHANGE()
     else
+        g.ctrl = 0
         local indunCls = GetClass('GuildEvent', 'GM_BorutosKapas_1');
 
         if indunCls ~= nil then
-            -- gewframe:ShowWindow(0)
+            -- frame:ShowWindow(0)
             local indunClsID = TryGetProp(indunCls, 'ClassID', 0);
             _BORUTA_ZONE_MOVE_CLICK(indunClsID)
         end
     end
 end
 
-function GUILDEVENTWARP_ON_GILTINE_CLICK(gewframe)
+function GUILDEVENTWARP_ON_GILTINE_CLICK(frame)
     -- CHAT_SYSTEM("ギルティネボタンがクリックされました")
     local channelnum = session.loginInfo.GetChannel() + 1;
     if channelnum ~= 1 then
+        g.ctrl = 2
         GUILDEVENTWARP_CH_CHANGE()
     else
+        g.ctrl = 0
         local indunCls = GetClass('GuildEvent', 'GM_Giltine_1');
 
         if indunCls ~= nil then
-            -- gewframe:ShowWindow(0)
+            -- frame:ShowWindow(0)
             local indunClsID = TryGetProp(indunCls, 'ClassID', 0);
             _BORUTA_ZONE_MOVE_CLICK(indunClsID)
         end
     end
 end
 
-function GUILDEVENTWARP_ON_BAUBAS_CLICK(gewframe)
+function GUILDEVENTWARP_ON_BAUBAS_CLICK(frame)
     -- CHAT_SYSTEM("バウバスボタンがクリックされました")
     local channelnum = session.loginInfo.GetChannel() + 1;
     if channelnum ~= 1 then
+        g.ctrl = 3
         GUILDEVENTWARP_CH_CHANGE()
     else
+        g.ctrl = 0
         local indunCls = GetClass('GuildEvent', 'GM_Baubas_1');
 
         if indunCls ~= nil then
-            -- gewframe:ShowWindow(0)
+            -- frame:ShowWindow(0)
             local indunClsID = TryGetProp(indunCls, 'ClassID', 0);
             _BORUTA_ZONE_MOVE_CLICK(indunClsID)
         end
@@ -114,3 +131,61 @@ function GUILDEVENTWARP_CH_CHANGE()
     end
 end
 
+--[[　元のコード
+function GUILDEVENTWARP_ON_BORUTA_CLICK(frame)
+    -- CHAT_SYSTEM("ボルタボタンがクリックされました")
+    local channelnum = session.loginInfo.GetChannel() + 1;
+    if channelnum ~= 1 then
+        GUILDEVENTWARP_CH_CHANGE()
+    else
+        local indunCls = GetClass('GuildEvent', 'GM_BorutosKapas_1');
+
+        if indunCls ~= nil then
+            -- frame:ShowWindow(0)
+            local indunClsID = TryGetProp(indunCls, 'ClassID', 0);
+            _BORUTA_ZONE_MOVE_CLICK(indunClsID)
+        end
+    end
+end
+
+function GUILDEVENTWARP_ON_GILTINE_CLICK(frame)
+    -- CHAT_SYSTEM("ギルティネボタンがクリックされました")
+    local channelnum = session.loginInfo.GetChannel() + 1;
+    if channelnum ~= 1 then
+        GUILDEVENTWARP_CH_CHANGE()
+    else
+        local indunCls = GetClass('GuildEvent', 'GM_Giltine_1');
+
+        if indunCls ~= nil then
+            -- frame:ShowWindow(0)
+            local indunClsID = TryGetProp(indunCls, 'ClassID', 0);
+            _BORUTA_ZONE_MOVE_CLICK(indunClsID)
+        end
+    end
+end
+
+function GUILDEVENTWARP_ON_BAUBAS_CLICK(frame)
+    -- CHAT_SYSTEM("バウバスボタンがクリックされました")
+    local channelnum = session.loginInfo.GetChannel() + 1;
+    if channelnum ~= 1 then
+        GUILDEVENTWARP_CH_CHANGE()
+    else
+        local indunCls = GetClass('GuildEvent', 'GM_Baubas_1');
+
+        if indunCls ~= nil then
+            -- frame:ShowWindow(0)
+            local indunClsID = TryGetProp(indunCls, 'ClassID', 0);
+            _BORUTA_ZONE_MOVE_CLICK(indunClsID)
+        end
+    end
+end
+
+function GUILDEVENTWARP_CH_CHANGE()
+    local channelID = 0 -- 0が1chらしい
+    local channelnum = session.loginInfo.GetChannel() + 1;
+    if channelnum ~= 1 then
+        -- CHAT_SYSTEM(channelnum)
+        RUN_GAMEEXIT_TIMER("Channel", channelID);
+    end
+end
+]]
