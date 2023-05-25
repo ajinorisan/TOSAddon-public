2fa28a762d9b7e71ae9acadf953e229296e0f91d
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-05-25 17:45:47 +0900

    a

--- _prototype/freefromlittlestress/freefromlittlestress/freefromlittlestress.lua
+++ _prototype/freefromlittlestress/freefromlittlestress/freefromlittlestress.lua
@@ -12,25 +12,28 @@ local acutil = require("acutil")
 
 g.settingsFileLoc = string.format('../addons/%s/settings.json', addonNameLower)
 
-g.defaultsettings = {
+g.settings = {
     rrfp_x = 500,
     rrfp_y = 300
-};
+}
 
 function FREEFROMLITTLESTRESS_SAVE_SETTINGS()
-    CHAT_SYSTEM("save")
+    -- CHAT_SYSTEM("save")
     acutil.saveJSON(g.settingsFileLoc, g.settings);
 
 end
 
 function FREEFROMLITTLESTRESS_LOADSETTINGS()
-    local settings, err = acutil.loadJSON(g.settingsFileLoc, g.defaultsettings)
+
+    local settings, err = acutil.loadJSON(g.settingsFileLoc, g.settings)
+
     if err then
         -- 設定ファイル読み込み失敗時処理
-        CHAT_SYSTEM(string.format("[%s] cannot load setting files", addonNameLower))
+        CHAT_SYSTEM(string.format("[%s] cannot load setting files",
+                                  addonNameLower))
     end
     if not settings then
-        settings = g.defaultSettings -- 変数名が `g.defaultSettings` ではなく `g.defaultsettings` になっているため修正
+        settings = g.settings
     end
 
     g.settings = settings
@@ -66,6 +69,8 @@ function FREEFROMLITTLESTRESS_RAID_RECORD_INIT(frame)
     frame:SetOffset(g.settings.rrfp_x, g.settings.rrfp_y)
     -- frame:SetOffset(500, 300)
     frame:SetSkinName("shadow_box")
+    frame:SetEventScript(ui.LBUTTONUP, "FREEFROMLITTLESTRESS_UPDATESETTINGS")
+    FREEFROMLITTLESTRESS_UPDATESETTINGS(frame)
 
     local myInfo = GET_CHILD_RECURSIVELY(frame, "myInfo")
     local myInfo_name = GET_CHILD_RECURSIVELY(myInfo, "name")
@@ -94,47 +99,24 @@ function FREEFROMLITTLESTRESS_RAID_RECORD_INIT(frame)
     GET_CHILD_RECURSIVELY(frame, 'bgIndunClear'):ShowWindow(1)
     GET_CHILD_RECURSIVELY(frame, 'textNewRecord'):ShowWindow(0);
 
-    -- FREEFROMLITTLESTRESS_UPDATESETTINGS(frame)
-
 end
---[[
--- タブ変えた時にスロットクリアーなるの嫌
-function FREEFROMLITTLESTRESS_CLEAR_GODDESS_EQUIP_MANAGER(frame)
-    CHAT_SYSTEM("test")
-    GODDESS_MGR_REFORGE_CLEAR(frame)
-    GODDESS_MGR_RANDOMOPTION_CLEAR(frame)
-    -- GODDESS_MGR_SOCKET_CLEAR(frame)
-    GODDESS_MGR_MAKE_CLEAR(frame)
-    GODDESS_MGR_INHERIT_CLEAR(frame)
-    GODDESS_MGR_CONVERT_CLEAR(frame)
-end
-]]
 
 -- 死んだ時に現れるフレームを移動可能に
 function FREEFROMLITTLESTRESS_FRAME_MOVE()
 
     local rcframe = ui.GetFrame("restart_contents") -- フレームを移動可能に設定する
     rcframe:EnableMove(1)
-    --[[
-    rcframe:SetSkinName("shadow_box")
-    local buttonSkin = "chat_window" -- 適用したいスキンの名前
-    local buttonNames = {"btn_restart_1", "btn_restart_2", "btn_restart_3", "btn_restart_4", "btn_restart_5"}
 
-    for i, buttonName in ipairs(buttonNames) do
-        local button = GET_CHILD_RECURSIVELY(rcframe, buttonName)
-        if button ~= nil then
-            button:SetSkinName(buttonSkin)
-        end
-    end
-    ]]
+    -- 多分コロニー時はこっちちゃうかな
     local rframe = ui.GetFrame("restart") -- フレームを移動可能に設定する
     rframe:EnableMove(1)
-    rframe:SetSkinName("shadow_box")
+    rframe:SetSkinName("None")
     local buttonSkin = "chat_window" -- 適用したいスキンの名前
-    local buttonNames = {"btn_restart_1", "btn_restart_2", "btn_restart_3", "btn_restart_4", "btn_restart_5"}
+    local buttonNames = {"btn_restart_1", "btn_restart_2", "btn_restart_3",
+                         "btn_restart_4", "btn_restart_5"}
 
     for i, buttonName in ipairs(buttonNames) do
-        local button = GET_CHILD_RECURSIVELY(rcframe, buttonName)
+        local button = GET_CHILD_RECURSIVELY(rframe, buttonName)
         if button ~= nil then
             button:SetSkinName(buttonSkin)
         end
@@ -142,7 +124,7 @@ function FREEFROMLITTLESTRESS_FRAME_MOVE()
 
 end
 -- エーテルジェム自動着脱作りかけ
-function FREEFROMLITTLESTRESS_FRAME_INIT()
+function g.FRAME_INIT()
     local invframe = ui.GetFrame('inventory')
     local inventoryGbox = invframe:GetChild("inventoryGbox")
 
@@ -150,48 +132,39 @@ function FREEFROMLITTLESTRESS_FRAME_INIT()
     local buttonX = inventoryGbox:GetWidth() - 240
     local buttonY = inventoryGbox:GetHeight() - 610
 
-    local eqbutton = inventoryGbox:CreateOrGetControl("button", "eqbutton", buttonX, buttonY, 50, 30)
+    local eqbutton = inventoryGbox:CreateOrGetControl("button", "eqbutton",
+                                                      buttonX, buttonY, 50, 30)
     eqbutton:SetText("equip")
 
     local rmbuttonX = inventoryGbox:GetWidth() - 105
     local rmbuttonY = inventoryGbox:GetHeight() - 610
 
-    local rmeqbutton = inventoryGbox:CreateOrGetControl("button", "rmeqbutton", rmbuttonX, rmbuttonY, 60, 30)
+    local rmeqbutton = inventoryGbox:CreateOrGetControl("button", "rmeqbutton",
+                                                        rmbuttonX, rmbuttonY,
+                                                        60, 30)
     rmeqbutton:SetText("rmequip")
 
-    eqbutton:SetEventScript(ui.LBUTTONUP, "ON_EQUIP_BUTTON_CLICK")
-    rmeqbutton:SetEventScript(ui.LBUTTONUP, "ON_RMEQUIP_BUTTON_CLICK")
+    eqbutton:SetEventScript(ui.LBUTTONUP, "g.ON_EQUIP_BUTTON_CLICK")
+    rmeqbutton:SetEventScript(ui.LBUTTONUP, "g.ON_RMEQUIP_BUTTON_CLICK")
 
 end
 
-function ON_EQUIP_BUTTON_CLICK(invframe)
+function g.ON_EQUIP_BUTTON_CLICK(invframe)
     print("equipボタンがクリックされました")
 
-    local goddessframe = ui.GetFrame('goddess_equip_manager')
+    local gdsframe = ui.GetFrame('goddess_equip_manager')
     local socket_bg = GET_CHILD_RECURSIVELY(goddessframe, "socket_bg")
 
 end
 
-function ON_RMEQUIP_BUTTON_CLICK(invframe)
+function g.ON_RMEQUIP_BUTTON_CLICK(invframe)
     print("rmequipボタンがクリックされました")
-    local enable_slot_list = {'RH', 'LH', 'RH_SUB', 'LH_SUB'}
-    for i = 1, #enable_slot_list do
-        local slot_name = enable_slot_list[i]
-        local ctrl_slot = GET_CHILD_RECURSIVELY(invframe, slot_name)
-        if ctrl_slot ~= nil then
-            local item = GET_SLOT_ITEM(ctrl_slot)
-            if item ~= nil then
-                local itemName = item:GetName()
-                local itemID = item:GetIESID()
-                print("スロット名: " .. slot_name)
-                print("アイテム名: " .. itemName)
-                print("アイテムID: " .. itemID)
-            else
-                print("スロット名 " .. slot_name .. " にはアイテムが入っていません")
-            end
-        else
-            print("スロット名 " .. slot_name .. " は見つかりませんでした")
-        end
-    end
+    local gdsframe = ui.GetFrame('goddess_equip_manager')
+    g.GODDESS_MGR_SOCKET_OPEN(gdsframe)
+
 end
 
+function g.GODDESS_MGR_SOCKET_OPEN(gdsframe)
+    _G.INVENTORY_SET_CUSTOM_RBTNDOWN('GODDESS_MGR_SOCKET_INV_RBTN')
+    _G.GODDESS_MGR_SOCKET_CLEAR(frame)
+end
