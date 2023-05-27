7ab4e85e920e4b71ea4057eda9a31d73234fd3e1
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-05-27 12:45:30 +0900

    a

--- klcount/klcount/klcount.lua
+++ klcount/klcount/klcount.lua
@@ -1,107 +1,138 @@
 local addonName = "KLCOUNT"
 local addonNameLower = string.lower(addonName)
 local author = "norisan"
-local ver = "1.0.2"
+local ver = "1.0.3"
 
 _G["ADDONS"] = _G["ADDONS"] or {}
 _G["ADDONS"][author] = _G["ADDONS"][author] or {}
 _G["ADDONS"][author][addonName] = _G["ADDONS"][author][addonName] or {}
 local g = _G["ADDONS"][author][addonName]
 
-g.settingsDirLoc = string.format("../addons/%s", addonNameLower)
-g.settingsFileLoc = string.format("%s/settings.json", g.settingsDirLoc)
+local acutil = require("acutil")
 
-local count = 0
+g.settingsFileLoc = string.format("../addons/%s/settings.json", addonNameLower);
 
-local targetMap = {
-    11260, -- 懺悔の祈祷室
-    11259, -- ノヴァハ書庫
-    11258, -- ノヴァハ本院
-    11255, -- 魔法結社の議事堂
-    11258, -- 隠し通路
-    11258  -- デルムーア待合室
+g.settings = {
+    f_x = 450,
+    f_y = 20
+}
+g.count = 0
+
+local targetMap = {11260, -- 懺悔の祈祷室
+11259, -- ノヴァハ書庫
+11258, -- ノヴァハ本院
+11255, -- 魔法結社の議事堂
+11254, -- 隠し通路
+11253 -- デルムーア待合室
 }
 
---CHAT_SYSTEM(imcTime.GetAppTimeMS())
+function KLCOUNT_SAVE_SETTINGS()
+    -- CHAT_SYSTEM("save")
+    acutil.saveJSON(g.settingsFileLoc, g.settings);
 
-function _G.KLCOUNT_ON_INIT(addon, frame)
-    --CancelReserveScript("_G.KLCOUNT_TIME_UPDATE()")
-    _G.CHAT_SYSTEM("KLCOUNT_ON_INIT")
-    g.addon = addon
-    g.frame = frame
-    
-    g.starttime =imcTime.GetAppTimeMS()
-    --g.starttime = 11773799
-    addon:RegisterMsg("GAME_START_3SEC", "KLCOUNT_INIT_FRAME")
-    addon:RegisterMsg("CHANGE_MAP", "KLCOUNT_INIT_FRAME")
-    addon:RegisterMsg("EXP_UPDATE", "KLCOUNT_UPDATE")
-    addon:RegisterMsg("FPS_UPDATE", "KLCOUNT_TIME_UPDATE")
-    --KLCOUNT_TIME_UPDATE()
-    --ReserveScript("_G.KLCOUNT_TIME_UPDATE()", 1.0)
-    
 end
 
-function _G.KLCOUNT_INIT_FRAME()
+function KLCOUNT_LOADSETTINGS()
+
+    local settings, err = acutil.loadJSON(g.settingsFileLoc, g.settings)
+
+    if err then
+        -- 設定ファイル読み込み失敗時処理
+        CHAT_SYSTEM(string.format("[%s] cannot load setting files", addonNameLower))
+    end
+    if not settings then
+        settings = g.settings
+    end
+
+    g.settings = settings
+end
+
+function KLCOUNT_UPDATESETTINGS(frame)
+    if g.settings.f_x ~= frame:GetX() or g.settings.f_y ~= frame:GetY() then
+        g.settings.f_x = frame:GetX()
+        g.settings.f_y = frame:GetY()
+        KLCOUNT_SAVE_SETTINGS()
+    end
+end
+
+function _G.KLCOUNT_ON_INIT(addon, frame)
+
+    _G.CHAT_SYSTEM(addonNameLower .. " loaded")
+    g.addon = addon
+    g.frame = frame
+
+    g.starttime = imcTime.GetAppTimeMS()
+
+    addon:RegisterMsg("GAME_START_3SEC", "KLCOUNT_INIT_FRAME")
+    -- addon:RegisterMsg("GAME_START_3SEC", "KLCOUNT_UPDATE")
+    addon:RegisterMsg("GAME_START_3SEC", "KLCOUNT_TIME_UPDATE")
+    addon:RegisterMsg("EXP_UPDATE", "KLCOUNT_UPDATE")
+
+    if g.IsTargetMap() == true then
+        addon:RegisterMsg("FPS_UPDATE", "KLCOUNT_TIME_UPDATE")
+    end
+
+    KLCOUNT_LOADSETTINGS()
+end
+
+function KLCOUNT_INIT_FRAME()
     local frame = _G.ui.GetFrame("klcount")
-     --対象外マップ時は非表示
+    -- 対象外マップ時は非表示
     if g.IsTargetMap() == false then
         frame:ShowWindow(0)
         return
     end
 
     frame:ShowWindow(1)
-    frame:Resize(250, 95)
-    frame:SetOffset(450, 20)
+    frame:EnableHitTest(1)
+    frame:EnableMove(1)
+    frame:Resize(170, 95)
+    if g.settings.f_x ~= nil then
+        frame:SetOffset(g.settings.f_x, g.settings.f_y)
+    else
+        frame:SetOffset(450, 20)
+    end
     frame:SetAlpha(80)
-    frame:EnableHitTest(0)
     frame:SetLayerLevel(31)
     frame:SetTitleBarSkin("shadow_box")
+    frame:SetEventScript(ui.LBUTTONUP, "KLCOUNT_UPDATESETTINGS")
 
-    count = 0
+    g.count = 0
 
     local countText = frame:CreateOrGetControl("richtext", "count_text", 10, 10, 200, 30)
-    countText:SetText(string.format("{s18}モンスター退治数: %d{/}", count))
+    -- local klStr = "KLCounter"
+    countText:SetText(string.format("{s16}KLCounter : %d{/}", g.count))
 
     local mapName = g.GetMapName()
     local mapText = frame:CreateOrGetControl("richtext", "map_text", 10, 35, 200, 30)
-    mapText:SetText(string.format("{s18}%s{/}", mapName))
-    
-    local timer = frame:CreateOrGetControl("richtext", "timer_text", 140, 60, 200, 30)
+    mapText:SetText(string.format("{s16}%s{/}", mapName))
+
+    local timer = frame:CreateOrGetControl("richtext", "timer_text", 90, 60, 200, 30)
     local h = 0
     local m = 0
-    local s = 0
-    timer:SetText(string.format("{s18}%02d:%02d:%02d{/}", h, m, s))
-    KLCOUNT_TIME_UPDATE()
-    --ReserveScript("_G.KLCOUNT_TIME_UPDATE()", 1.0)
+    local s = 3
+    timer:SetText(string.format("{s16}%02d:%02d:%02d{/}", h, m, s))
+    KLCOUNT_TIME_UPDATE(frame)
+    -- KLCOUNT_UPDATE(frame)
+
 end
 
-function KLCOUNT_TIME_UPDATE()
+function KLCOUNT_TIME_UPDATE(frame)
     local time = imcTime.GetAppTimeMS() - g.starttime
     local h = math.floor(time / (60 * 60 * 1000))
     local m = math.floor((time / (60 * 1000)) % 60)
     local s = math.floor((time / 1000) % 60)
-    local frame = _G.ui.GetFrame("klcount")
-    local timer = frame:CreateOrGetControl("richtext", "timer_text", 140, 60, 200, 30)
-    timer:SetText(string.format("{s18}%02d:%02d:%02d{/}", h, m, s))
-    --ReserveScript("KLCOUNT_TIME_UPDATE()",1)
-    --_G.KLCOUNT_TIME_ON()
+    local timer = GET_CHILD_RECURSIVELY(frame, "timer_text")
+    timer:SetText(string.format("{s16}%02d:%02d:%02d{/}", h, m, s))
 end
 
---function _G.KLCOUNT_TIME_ON()
-    --ui.sleep(1000)
-    --_G.KLCOUNT_TIME_UPDATE()
---end
-
-function _G.KLCOUNT_UPDATE()
+function KLCOUNT_UPDATE(frame)
     if g.IsTargetMap() == false then
         return
     end
-
-    local frame = _G.ui.GetFrame("klcount")
-    local countText = frame:CreateOrGetControl("richtext", "count_text", 10, 10, 200, 30)
-
-    count = count + 1
-    countText:SetText(string.format("{s18}モンスター退治数: %d{/}", count))
+    local countText = GET_CHILD_RECURSIVELY(frame, "count_text")
+    g.count = g.count + 1
+    countText:SetText(string.format("{s16}KLCounter : %d{/}", g.count))
 end
 
 -- 対象マップか判定。対象マップtrue、対象外マップfalse
