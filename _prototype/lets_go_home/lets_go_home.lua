cd2d0a0ed8c7c8fe1ad1f45ea25bebda6df677a8
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-08-08 17:19:01 +0900

    a

---
+++ _prototype/lets_go_home/lets_go_home.lua
@@ -0,0 +1,21 @@
+local addonName = "LETS_GO_HOME"
+local addonNameLower = string.lower(addonName)
+local author = "norisan"
+local ver = "0.0.1"
+
+_G["ADDONS"] = _G["ADDONS"] or {}
+_G["ADDONS"][author] = _G["ADDONS"][author] or {}
+_G["ADDONS"][author][addonName] = _G["ADDONS"][author][addonName] or {}
+local g = _G["ADDONS"][author][addonName]
+
+g.settingsFileLoc = string.format('../addons/%s/settings.json', addonNameLower)
+
+local acutil = require("acutil")
+
+function LETS_GO_HOME_ON_INIT(addon, frame)
+
+    g.addon = addon
+    g.frame = frame
+
+end
+
