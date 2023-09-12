e1e5338d897c6d7c24cb5495dceaa85cd8237cf9
norisan <128560971+ajinorisan@users.noreply.github.com>
2023-09-12 21:42:04 +0900

    bossdirection 1.0.0

--- boss_direction/boss_direction/boss_direction.lua
+++ boss_direction/boss_direction/boss_direction.lua
@@ -30,46 +30,49 @@ function BOSS_DIRECTION_ON_INIT(addon, frame)
     g.frame = frame
     g.framename = addonNameLower
 
-    addon:RegisterMsg('TARGET_SET_BOSS', 'BOSS_DIRECTION_ON_MSG');
-    addon:RegisterMsg('TARGET_BUFF_UPDATE', 'BOSS_DIRECTION_ON_MSG');
-    addon:RegisterMsg('TARGET_CLEAR_BOSS', 'BOSS_DIRECTION_ON_MSG');
-    addon:RegisterMsg('TARGET_UPDATE', 'BOSS_DIRECTION_ON_MSG');
-
     local pc = GetMyPCObject();
     local curMap = GetZoneName(pc)
     local mapCls = GetClass("Map", curMap)
     if mapCls.MapType ~= "City" then
-        addon:RegisterMsg('FPS_UPDATE', 'BOSS_DIRECTION_ON_MSG');
+
+        addon:RegisterMsg('TARGET_SET_BOSS', 'BOSS_DIRECTION_ON_MSG');
+        addon:RegisterMsg('TARGET_BUFF_UPDATE', 'BOSS_DIRECTION_ON_MSG');
+        addon:RegisterMsg('TARGET_CLEAR_BOSS', 'BOSS_DIRECTION_ON_MSG');
+        addon:RegisterMsg('TARGET_UPDATE', 'BOSS_DIRECTION_ON_MSG');
+        local frame = ui.GetFrame(g.framename)
+        frame:RunUpdateScript("BOSS_DIRECTION_ON_MSG", 0.1)
     end
 
 end
 
 function BOSS_DIRECTION_ON_MSG(frame, msg, argStr, argNum)
-
     local handle = session.GetTargetBossHandle();
     local targetinfo = info.GetTargetInfo(handle);
     local frame = ui.GetFrame(g.framename)
+    frame:SetSkinName("None")
+    frame:SetTitleBarSkin("None")
 
-    local scale = 3.0
-    frame:Resize(200 * scale, 220 * scale);
+    frame:Resize(150, 150);
     frame:SetLayerLevel(91)
 
-    local picCircle = frame:GetChild("point_circle");
-    if picCircle == nil then
-        picCircle = frame:CreateOrGetControl("picture", "point_circle", 0, 0, 220, 220);
-        tolua.cast(picCircle, "ui::CPicture");
-        -- picCircle:SetImage("boss_direction");
-        picCircle:SetImage("class_tree_arrow")
-        picCircle:SetEnableStretch(1);
-        picCircle:EnableHitTest(0);
-        picCircle:SetGravity(ui.CENTER_HORZ, ui.CENTER_VERT);
+    local arrow = frame:GetChild("arrow");
+    if arrow == nil then
+        arrow = frame:CreateOrGetControl("picture", "arrow", 0, 0, 80, 80);
+        tolua.cast(arrow, "ui::CPicture");
+        -- arrow:SetImage("gold_arrow_right");
+        arrow:SetImage("class_tree_arrow")
+        arrow:SetEnableStretch(1);
+        arrow:EnableHitTest(0);
+        arrow:SetGravity(ui.CENTER_HORZ, ui.CENTER_VERT);
     end
-    local assistCurrentX = 45
-    picCircle:SetAngle(info.GetAngle(handle) + assistCurrentX);
-    picCircle:Resize(220 * scale, 220 * scale);
-    picCircle:SetColorTone("FF7777FF");
-    FRAME_AUTO_POS_TO_OBJ(frame, handle, -frame:GetWidth() / 2, -frame:GetHeight() / 2, 0, 0);
-    frame:ShowWindow(1);
-    return
+    if handle ~= 0 then
+        arrow:SetAngle(info.GetAngle(handle) - 30);
+        arrow:Resize(130, 130);
+        arrow:SetColorTone("FFFF0000");
+        FRAME_AUTO_POS_TO_OBJ(frame, handle, -frame:GetWidth() / 2, -frame:GetHeight() / 2, 0, 0);
+        frame:ShowWindow(1);
+    end
+    return 1
+
 end
 
