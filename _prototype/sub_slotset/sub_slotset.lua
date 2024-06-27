cbd7bb08062af6218e1a3b1b8aff2ff7d1b76c5d
norisan <128560971+ajinorisan@users.noreply.github.com>
2024-06-27 17:48:05 +0900

    a

--- _prototype/sub_slotset/sub_slotset.lua
+++ _prototype/sub_slotset/sub_slotset.lua
@@ -33,139 +33,115 @@ function SUB_SLOTSET_ON_INIT(addon, frame)
     g.settings = g.settings or {}
 
     addon:RegisterMsg("GAME_START", "sub_slotset_frame_init")
-    acutil.setupEvent(addon, "CHAT_EMOTICON_MAKELIST", "sub_slotset_CHAT_EMOTICON_MAKELIST");
-    -- g.SetupHook(market_voucher_CABINET_GET_ALL_LIST, "CABINET_GET_ALL_LIST");
-end
-
-function sub_slotset_CHAT_EMOTICON_MAKELIST(msg)
-    ReserveScript("sub_slotset_CHAT_EMOTICON_MAKELIST_()", 0.2)
-
-end
-
-function sub_slotset_CHAT_EMOTICON_MAKELIST_()
-    local frame = ui.GetFrame("chat_emoticon")
-    local emoticons = GET_CHILD_RECURSIVELY(frame, "emoticons")
-    local slotCount = emoticons:GetSlotCount();
-    local count = 0
-    for i = 1, slotCount do
-        local slot = GET_CHILD_RECURSIVELY(emoticons, "slot" .. i)
-        local icon = slot:GetIcon()
-        if icon ~= nil then
-            count = count + 1
-        end
-    end
-    emoticons:SetColRow(emoticons:GetCol(), math.ceil(count / 10))
-    emoticons:CreateSlots()
-    -- local gbox = GET_CHILD_RECURSIVELY(frame, "gbox")
 
 end
 
 function sub_slotset_frame_init()
-    local frame = ui.GetFrame("quickslotnexpbar")
-    local emoticon = frame:CreateOrGetControl("button", "emoticon", 0, 0, 20, 20)
+    local frame = ui.GetFrame("sub_slotset")
+    frame:RemoveAllChild()
+    frame:SetSkinName("None")
+    frame:SetTitleBarSkin("None")
+    frame:Resize(35, 35)
+    frame:SetPos(790, 2)
+    frame:SetLayerLevel(10);
 
-    AUTO_CAST(emoticon)
-    emoticon:SetText("{ol}{s11}E")
-    emoticon:SetGravity(ui.RIGHT, ui.BOTTOM)
-    emoticon:SetMargin(0, 0, 0, 0);
-    emoticon:SetEventScript(ui.LBUTTONUP, "sub_slotset_emoticon")
+    local slot = frame:CreateOrGetControl('slot', 'slot', 0, 0, 30, 30)
+    AUTO_CAST(slot)
+    slot:SetSkinName("None");
+    slot:EnablePop(0)
+    slot:EnableDrop(0)
+    slot:EnableDrag(0)
+    slot:SetEventScript(ui.LBUTTONUP, "sub_slotset_make_frame");
 
-    local move_emoticon = frame:CreateOrGetControl("button", "move_emoticon", 0, 0, 20, 20)
-    AUTO_CAST(move_emoticon)
-    move_emoticon:SetText("{ol}{s11}E")
-    move_emoticon:SetGravity(ui.RIGHT, ui.BOTTOM)
-    move_emoticon:SetMargin(0, -25, 0, 0);
+    local icon = CreateIcon(slot);
+    AUTO_CAST(icon)
+    icon:SetImage("btn_plus");
+    frame:ShowWindow(1)
+end
+
+function sub_slotset_set_edit(frame, ctrl, str, num)
+    local ctrl_name = ctrl:GetName()
+    local ctrl_type = type(ctrl:GetText())
+    if ctrl_type ~= "number" then
+        ui.SysMsg("Numeric input")
+        return
+    end
+    if tonumber(ctrl:GetText()) <= 10 then
+        ui.SysMsg("Enter less than 10")
+        return
+    end
+    if ctrl_name == "column_edit" then
+        g.column = tonumber(ctrl:GetText())
+    elseif ctrl_name == "row_edit" then
+        g.row = tonumber(ctrl:GetText())
+    end
+end
+
+function sub_slotset_make_frame(frame)
+    frame:Resize(300, 300)
+    frame:SetSkinName("None")
+    frame:SetTitleBarSkin("None")
+    frame:SetLayerLevel(91)
+
+    local gbox = frame:CreateOrGetControl("groupbox", "gbox", 35, 0, frame:GetWidth() - 35, frame:GetHeight())
+    AUTO_CAST(gbox)
+    gbox:SetSkinName("test_frame_midle_light")
+
+    local column = gbox:CreateOrGetControl("richtext", "column", 10, 5, 50, 25)
+    AUTO_CAST(column)
+    column:SetText("{ol}column")
+
+    local column_edit = gbox:CreateOrGetControl('edit', 'column_edit', 10, 35, 50, 25)
+    AUTO_CAST(column_edit)
+    column_edit:SetFontName('white_16_ol')
+    column_edit:SetSkinName('test_weight_skin')
+    column_edit:SetTextAlign('center', 'center')
+    column_edit:SetEventScript(ui.ENTERKEY, "sub_slotset_set_edit");
+    column_edit:SetText(g.column or tonumber(3))
+
+    local row = gbox:CreateOrGetControl("richtext", "row", 65, 5, 50, 25)
+    AUTO_CAST(row)
+    row:SetText("{ol}row")
+
+    local row_edit = gbox:CreateOrGetControl('edit', 'row_edit', 65, 35, 50, 25)
+    AUTO_CAST(row_edit)
+    row_edit:SetFontName('white_16_ol')
+    row_edit:SetSkinName('test_weight_skin')
+    row_edit:SetTextAlign('center', 'center')
+    row_edit:SetEventScript(ui.ENTERKEY, "sub_slotset_set_edit");
+    column_edit:SetText(g.row or tonumber(3))
+
+    local make = gbox:CreateOrGetControl('button', 'make', 10, 75, 50, 25)
+    AUTO_CAST(make)
+    make:SetSkinName("test_red_button")
+    make:SetEventScript(ui.LBUTTONUP, "sub_slotset_make_context");
+
+    local cancel = gbox:CreateOrGetControl('button', 'cancel', 65, 75, 50, 25)
+    AUTO_CAST(cancel)
+    cancel:SetSkinName("test_gray_button")
+    cancel:SetEventScript(ui.LBUTTONUP, "sub_slotset_frame_init");
+
+    --[[local public_text = gbox:CreateOrGetControl("richtext", "public_text", 10, 80, 50, 25)
+    AUTO_CAST(public_text)
+    public_text:SetText("{ol}Per character or shared.Shared when checked")
+
+    local public_check = frame:CreateOrGetControl("checkbox", "public_check", 90, 80, 25, 25)
+    AUTO_CAST(public_check)]]
+end
+
+function sub_slotset_make_context(frame, ctrl, str, num)
+
+    local context = ui.CreateContextMenu("make_context", "Shared or Character", 0, 0, 100, 100);
+    ui.AddContextMenuItem(context, " ", "None");
+    local scp = string.format("sub_slotset_make_slotset('%s')", "shared")
+    ui.AddContextMenuItem(context, "Make team shared slotset", scp);
+    scp = string.format("sub_slotset_make_slotset('%s')", "character")
+    ui.AddContextMenuItem(context, "Make character slotset", scp);
+    ui.OpenContextMenu(context);
 
 end
 
-function sub_slotset_emoticon(frame, ctrl, str, num)
-
-    local frame = ui.GetFrame("chat_emoticon")
-    local list, listCnt = GetClassList("chat_emoticons")
-    -- local emoticons = GET_CHILD_RECURSIVELY(frame, "emoticons")
-    local emoticons = frame:CreateOrGetControl('slotset', 'slotset', 0, 0, 0, 0)
-    AUTO_CAST(emoticons);
-    emoticons:SetSlotSize(42, 42) -- スロットの大きさ
-    emoticons:EnablePop(0)
-    emoticons:EnableDrag(0)
-    emoticons:EnableDrop(0)
-    local emoticons_row = math.ceil(listCnt / 10)
-    emoticons:SetColRow(10, emoticons_row)
-    emoticons:SetSpc(0, 0)
-    emoticons:SetSkinName('invenslot')
-    emoticons:CreateSlots()
-
-    local cnt = emoticons:GetSlotCount()
-    local acc = GetMyAccountObj()
-    local index = 0
-    local list, listCnt = GetClassList("chat_emoticons")
-
-    -- 아이콘 타입 확인 : 일반, 모션
-    local iconGroup = frame:GetUserValue("EMOTICON_GROUP")
-    local curCnt = frame:GetUserIValue("CURCNT")
-    if iconGroup == "None" then
-        iconGroup = "Normal"
-    end
-
-    for i = 0, listCnt - 1 do
-        acc = GetMyAccountObj()
-        local slot = emoticons:GetSlotByIndex(index)
-        slot:SetEventScript(ui.MOUSEMOVE, "CHAT_EMOTICON_ADDDURATION")
-        slot:SetOverSound("button_over")
-        slot:SetClickSound("button_click_chat")
-        if index < cnt then
-            local cls = GetClassByIndexFromList(list, i)
-
-            if TryGetProp(cls, 'HaveUnit', 'None') == 'PC' then
-                acc = GetMyEtcObject()
-            else
-                acc = GetMyAccountObj()
-            end
-
-            if cls.IconGroup == iconGroup then
-                if cls.CheckServer == 'YES' then
-                    -- check session emoticons
-                    local haveEmoticon = TryGetProp(acc, 'HaveEmoticon_' .. cls.ClassID)
-                    if haveEmoticon > 0 then
-                        local icon = CreateIcon(slot)
-                        local namelist = StringSplit(cls.ClassName, "motion_")
-                        local imageName = namelist[1]
-                        if 1 < #namelist then
-                            imageName = namelist[2]
-                        end
-
-                        icon:SetImage(imageName)
-                        local tooltipText = string.format("%s%s", "/", cls.IconTokken)
-                        icon:SetTextTooltip(tooltipText)
-
-                        index = index + 1
-                        slot:ShowWindow(1)
-                    end
-                else
-                    local icon = CreateIcon(slot)
-                    local namelist = StringSplit(cls.ClassName, "motion_")
-                    local imageName = namelist[1]
-                    if 1 < #namelist then
-                        imageName = namelist[2]
-                    end
-
-                    icon:SetImage(imageName)
-                    local tooltipText = string.format("%s%s", "/", cls.IconTokken)
-                    icon:SetTextTooltip(tooltipText)
-                    index = index + 1
-                    slot:ShowWindow(1)
-                end
-            end
-        end
-    end
-
-    if curCnt ~= 0 then
-        for i = index, curCnt - 1 do
-            local slot = emoticons:GetSlotByIndex(i)
-            slot:ClearIcon()
-        end
-    end
-
-    frame:SetUserValue("CURCNT", index)
-
+function sub_slotset_make_slotset(str)
+    local newFrame = ui.CreateNewFrame("chatpopup", "chatpopup_" .. roomid)
 end
+
