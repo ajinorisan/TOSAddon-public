82b044fa9978702772c7bba10768567031cbe144
norisan <128560971+ajinorisan@users.noreply.github.com>
2025-01-14 17:46:50 +0900

    a

--- _prototype/vakarine_equip/vakarine_equip.lua
+++ _prototype/vakarine_equip/vakarine_equip.lua
@@ -1,11 +1,8 @@
 -- v1.0.0 とりあえず作った。
--- v1.0.1 いつものバグ修正。BUFF_REMOVEの際の挙動修正。
--- v1.0.2 richtextがslotに書き換わってた。なんでや妖精か？？
--- v1.0.3 早着替え機能のみ。バフ削除機能は封印
-local addonName = "no_heal"
+local addonName = "vakarine_equip"
 local addonNameLower = string.lower(addonName)
 local author = "norisan"
-local ver = "1.0.3"
+local ver = "1.0.0"
 
 _G["ADDONS"] = _G["ADDONS"] or {}
 _G["ADDONS"][author] = _G["ADDONS"][author] or {}
@@ -61,25 +58,19 @@ function g.mkdir_new_folder()
 end
 g.mkdir_new_folder()
 
-function no_heal_vakarine()
+function vakarine_equip_vakarine()
     local equip_item_list = session.GetEquipItemList();
     local equip_guid_list = equip_item_list:GetGuidList();
     local count = equip_guid_list:Count();
     local vakarine_count = 0
     for i = 0, count - 1 do
-
         local guid = equip_guid_list:Get(i);
-
         if guid ~= '0' then
-
             local equip_item = equip_item_list:GetItemByGuid(guid);
-
             if equip_item ~= nil and equip_item:GetObject() ~= nil then
 
                 local item = GetIES(equip_item:GetObject())
-
                 for j = 1, MAX_OPTION_EXTRACT_COUNT do
-
                     local propGroupName = "RandomOptionGroup_" .. j;
                     local propName = "RandomOption_" .. j;
                     local cls_msg = ScpArgMsg(item[propName])
@@ -87,7 +78,6 @@ function no_heal_vakarine()
                         vakarine_count = vakarine_count + 1
                         break
                     end
-
                 end
             end
         end
@@ -104,71 +94,72 @@ function no_heal_vakarine()
     end
 end
 
-function no_heal_save_settings()
+function vakarine_equip_save_settings()
     acutil.saveJSON(g.settingsFileLoc, g.settings)
 end
 
-function no_heal_load_settings()
+function vakarine_equip_load_settings()
 
     local settings, err = acutil.loadJSON(g.settingsFileLoc, g.settings)
-
     if not settings then
         settings = {}
-
     end
     g.settings = settings
-
     if not g.settings[tostring(g.cid)] then
         g.settings[tostring(g.cid)] = {
             use = 1,
-            equip_iesid = {},
-            buffid = {}
-
+            buffid = {},
+            equip_tbl = {
+                RH = 1,
+                LH = 1,
+                RH_SUB = 1,
+                LH_SUB = 1,
+                NECK = 1,
+                RING1 = 1,
+                RING2 = 1,
+                SHIRT = 1,
+                PANTS = 1,
+                GLOVES = 1,
+                BOOTS = 1,
+                SHOULDER = 1,
+                BELT = 1
+            }
         }
     end
-    --[[local check_equip_tbl = {8, 9, 30, 31, 19, 17, 18, 3, 14, 4, 5, 34, 33}
+    vakarine_equip_save_settings()
+    local check_equip_tbl = {8, 9, 30, 31, 19, 17, 18, 3, 14, 4, 5, 34, 33}
     local check_str_tbl = {"NoWeapon", "NoWeapon", "NoWeapon", "NoWeapon", "NoNeck", "NoRing", "NoRing", "NoShirt",
-                           "NoPants", "NoGloves", "NoBoots", "NoOuter", "NoOuter"}]]
-    --[[local equip_tbl = {"RH", "LH", "RH_SUB", "LH_SUB", "NECK", "RING1", "RING2", "SHIRT", "PANTS", "GLOVES", "BOOTS",
-                       "SHOULDER", "BELT"}]]
-    no_heal_save_settings()
-end
+                           "NoPants", "NoGloves", "NoBoots", "NoOuter", "NoOuter"}
+    local equips_tbl = {"RH", "LH", "RH_SUB", "LH_SUB", "NECK", "RING1", "RING2", "SHIRT", "PANTS", "GLOVES", "BOOTS",
+                        "SHOULDER", "BELT"}
 
-function no_heal_bufflist_close(frame, ctrl, str, num)
-    frame:ShowWindow(0)
-end
+    g.util_tbl = {}
 
-local check_equip_tbl = {8, 9, 30, 31, 19, 17, 18, 3, 14, 4, 5, 34, 33}
-local check_str_tbl = {"NoWeapon", "NoWeapon", "NoWeapon", "NoWeapon", "NoNeck", "NoRing", "NoRing", "NoShirt",
-                       "NoPants", "NoGloves", "NoBoots", "NoOuter", "NoOuter"}
+    for i, equip_name in ipairs(equips_tbl) do
+        if g.settings[tostring(g.cid)][equip_name] == 1 then
+            g.util_tbl[i] = {}
+            g.util_tbl[i][equip_name] = {}
+            local equip_index = check_equip_tbl[i]
+            local check_str = check_str_tbl[i]
+            g.util_tbl[i][equip_name][equip_index] = check_str
+        end
+    end
 
-local equip_tbl = {"RH", "LH", "RH_SUB", "LH_SUB", "NECK", "RING1", "RING2", "SHIRT", "PANTS", "GLOVES", "BOOTS",
-                   "SHOULDER", "BELT"}
-
--- unequip テーブルの作成
-local unequip = {}
-
--- check_equip_tbl と check_str_tbl を組み合わせる
-for i, equip_id in ipairs(check_equip_tbl) do
-    unequip[i] = {} -- i をキーとして新しいテーブルを作成
-    unequip[i][equip_id] = {} -- equip_id をキーとしてさらにネスト
-    local equip_name = equip_tbl[i] -- equip_tbl から対応する装備名を取得
-    local check_str = check_str_tbl[i] -- check_str_tbl から対応する文字列を取得
-    unequip[i][equip_id][equip_name] = check_str -- unequip テーブルに追加
-end
-
--- unequip テーブルの内容を表示
-for i, v in ipairs(unequip) do
-    print("Index " .. i .. ":") -- インデックスを表示
-    for equip_id, details in pairs(v) do
-        print("  Equip ID: " .. equip_id) -- equip_id を表示
-        for equip_name, status in pairs(details) do
-            print("    " .. equip_name .. " = " .. status) -- ネストされたテーブルの内容を表示
+    for i, equip in ipairs(g.util_tbl) do
+        for equip_name, equip_info in pairs(equip) do
+            for equip_index, check_str in pairs(equip_info) do
+                print(string.format("装備名: %s, 装備ID: %d, チェック文字列: %s", equip_name, equip_index,
+                                    check_str))
+            end
         end
     end
 end
 
-function no_heal_lbtn(frame, ctrl, str, num)
+function vakarine_equip_bufflist_close(frame, ctrl, str, num)
+    frame:ShowWindow(0)
+end
+
+function vakarine_equip_buff_list(frame, ctrl, str, num)
     local bufflistframe = ui.CreateNewFrame("notice_on_pc", addonNameLower .. "_bufflist", 0, 0, 10, 10)
     AUTO_CAST(bufflistframe)
     bufflistframe:SetSkinName("bg")
@@ -176,78 +167,62 @@ function no_heal_lbtn(frame, ctrl, str, num)
     bufflistframe:SetPos(10, 10)
     bufflistframe:SetLayerLevel(121)
     bufflistframe:RemoveAllChild()
-
     local bg = bufflistframe:CreateOrGetControl("groupbox", "bufflist_bg", 5, 35, 490, 1015)
     AUTO_CAST(bg)
     bg:SetSkinName("bg")
-
     local closeBtn = bufflistframe:CreateOrGetControl('button', 'closeBtn', 450, 0, 30, 30)
     AUTO_CAST(closeBtn)
     closeBtn:SetImage("testclose_button")
     closeBtn:SetGravity(ui.RIGHT, ui.TOP)
-    closeBtn:SetEventScript(ui.LBUTTONUP, "no_heal_bufflist_close");
-
+    closeBtn:SetEventScript(ui.LBUTTONUP, "vakarine_equip_bufflist_close");
     local sortedBuffIDs = {}
     for buffID, _ in pairs(g.settings[tostring(g.cid)]["buffid"]) do
         table.insert(sortedBuffIDs, tonumber(buffID))
     end
     table.sort(sortedBuffIDs)
-
     local bufflisttext = bufflistframe:CreateOrGetControl('richtext', 'bufflisttext', 90, 10, 200, 30)
     AUTO_CAST(bufflisttext)
-    bufflisttext:SetText("{ol}No Heal Buff List")
-
+    bufflisttext:SetText("{ol}Vakarine Equip Buff List")
     local y = 0
     local i = 1
-
     for _, buffID in ipairs(sortedBuffIDs) do
-
         local buffslot = bg:CreateOrGetControl('slot', 'buffslot' .. i, 10, y + 5, 30, 30)
         AUTO_CAST(buffslot)
         local buffCls = GetClassByType("Buff", buffID);
-
         if buffCls ~= nil then
             SET_SLOT_IMG(buffslot, GET_BUFF_ICON_NAME(buffCls));
-
             local icon = CreateIcon(buffslot)
             AUTO_CAST(icon)
             icon:SetTooltipType('buff');
             icon:SetTooltipArg(buffCls.Name, buffID, 0);
-
             local buffcheck = bg:CreateOrGetControl('checkbox', 'buffcheck' .. i, 45, y + 5, 30, 30)
             AUTO_CAST(buffcheck)
             local check = g.settings[tostring(g.cid)]["buffid"][tostring(buffID)] or 0
-
-            function no_heal_buff_check(frame, ctrl, argStr, buffID)
+            function vakarine_equip_buff_check(frame, ctrl, argStr, buffID)
                 local check = ctrl:IsChecked()
                 g.settings[tostring(g.cid)]["buffid"][tostring(buffID)] = check
-                no_heal_save_settings()
+                vakarine_equip_save_settings()
             end
-
             buffcheck:SetCheck(check)
-            buffcheck:SetEventScript(ui.LBUTTONUP, "no_heal_buff_check")
+            buffcheck:SetEventScript(ui.LBUTTONUP, "vakarine_equip_buff_check")
             buffcheck:SetEventScriptArgNumber(ui.LBUTTONUP, buffID)
-
             buffcheck:SetText("{ol}" .. buffCls.Name)
             buffcheck:SetTextTooltip(g.lang == "Japanese" and "{ol}" .. buffID ..
                                          "{nl}チェックするとバフを削除" or "{ol}" .. buffID ..
                                          "{nl}Check to remove buffs")
-
             y = y + 35
             i = i + 1
         end
-
     end
-
     bufflistframe:ShowWindow(1)
 end
 
-function no_heal_jobpic_lbtn(frame, ctrl, str, num)
+function vakarine_equip_pic_lbtn(frame, ctrl, str, num)
 
     local frame = ui.GetFrame("headsupdisplay")
     local jobPic = GET_CHILD_RECURSIVELY(frame, "jobPic")
-    local pic = jobPic:CreateOrGetControl("picture", "pic", 0, 0, 50, 50);
-    AUTO_CAST(pic)
+    local pic = GET_CHILD_RECURSIVELY(frame, "pic")
+
     pic:SetGravity(ui.RIGHT, ui.TOP);
     if ctrl ~= "GAME_START" then
         if g.settings[tostring(g.cid)].use == 0 then
@@ -256,39 +231,44 @@ function no_heal_jobpic_lbtn(frame, ctrl, str, num)
             pic:SetImage("itemslot_alchemy_mark")
             pic:SetEnableStretch(1);
             pic:EnableHitTest(1)
-            pic:SetEventScript(ui.LBUTTONUP, "no_heal_lbtn")
+            pic:SetColorTone("FFFFFFFF")
+            -- pic:SetEventScript(ui.LBUTTONUP, "vakarine_equip_buff_list")
         else
-            pic:ShowWindow(0)
+            pic:SetColorTone("FF555555")
             g.settings[tostring(g.cid)].use = 0
         end
-        no_heal_save_settings()
+        vakarine_equip_save_settings()
     else
         if g.settings[tostring(g.cid)].use == 0 then
-            pic:ShowWindow(0)
+            pic:SetColorTone("FF555555")
         else
             pic:ShowWindow(1)
             pic:SetImage("itemslot_alchemy_mark")
             pic:SetEnableStretch(1);
             pic:EnableHitTest(1)
-            pic:SetEventScript(ui.LBUTTONUP, "no_heal_lbtn")
+            pic:SetColorTone("FFFFFFFF")
+            -- pic:SetEventScript(ui.LBUTTONUP, "vakarine_equip_buff_list")
         end
     end
 end
 
-function no_heal_switching(frame, msg, str, num)
+function vakarine_equip_switching(frame, msg, str, num)
 
     local frame = ui.GetFrame("headsupdisplay")
     local jobPic = GET_CHILD_RECURSIVELY(frame, "jobPic")
     AUTO_CAST(jobPic)
-    no_heal_jobpic_lbtn(frame, msg, str, num)
-    jobPic:EnableHitTest(1)
-    jobPic:SetEventScript(ui.LBUTTONUP, "no_heal_jobpic_lbtn")
+    local pic = jobPic:CreateOrGetControl("picture", "pic", 0, 0, 50, 50);
+    AUTO_CAST(pic)
+    pic:EnableHitTest(1)
+    pic:SetEventScript(ui.LBUTTONUP, "vakarine_equip_pic_lbtn")
+    pic:SetTextTooltip(g.lang == "Japanese" and "{ol}Vakarine Equip{nl}左クリックでON/OFF切替" or
+                           "{ol}Vakarine Equip{nl}Left click to switch ON/OFF")
+    vakarine_equip_pic_lbtn(frame, msg, str, num)
 end
 
-function no_heal_BUFF_ON_MSG(frame, msg, str, buff_id)
+function vakarine_equip_BUFF_ON_MSG(frame, msg, str, buff_id)
     local exists = false
     for buffid, value in pairs(g.settings[tostring(g.cid)]["buffid"]) do
-
         if tonumber(buffid) == buff_id then
             exists = true
             if value == 1 then
@@ -297,14 +277,13 @@ function no_heal_BUFF_ON_MSG(frame, msg, str, buff_id)
             return
         end
     end
-
     if not exists then
         g.settings[tostring(g.cid)]["buffid"][tostring(buff_id)] = 0
-        no_heal_save_settings()
+        vakarine_equip_save_settings()
     end
 end
 
-function no_heal_animus_unequip(frame, msg, str, num)
+function vakarine_equip_animus_unequip(frame, msg, str, num)
 
     session.ResetItemList();
     local equipItemList = session.GetEquipItemList();
@@ -322,7 +301,7 @@ function no_heal_animus_unequip(frame, msg, str, num)
             if iesid ~= "0" then
                 item.UnEquip(spot_index)
                 if i ~= #unequip_tbl then
-                    ReserveScript("no_heal_animus_unequip()", 0.15)
+                    ReserveScript("vakarine_equip_animus_unequip()", 0.15)
                     return
                 end
             end
@@ -333,7 +312,7 @@ function no_heal_animus_unequip(frame, msg, str, num)
                        "SHOULDER", "BELT"}]]
     local equip_tbl = {"RH", "LH", "RH_SUB", "LH_SUB", "NECK", "RING1", "RING2", "SHIRT", "PANTS", "GLOVES", "BOOTS"}
 
-    function no_heal_item_equip()
+    function vakarine_equip_item_equip()
         for i, spot_name in ipairs(equip_tbl) do
             local iesid = g.settings[tostring(g.cid)]["equip_iesid"][spot_name]
 
@@ -351,15 +330,15 @@ function no_heal_animus_unequip(frame, msg, str, num)
                     ITEM_EQUIP(item_index, spot_name)
                     g.settings[tostring(g.cid)]["equip_iesid"][spot_name] = nil
                     if i ~= #equip_tbl then
-                        ReserveScript("no_heal_item_equip()", 0.2)
+                        ReserveScript("vakarine_equip_item_equip()", 0.2)
                         return
                     end
 
                 end
             end
             if i == #equip_tbl then
-                ReserveScript("no_heal_equips_check()", 0.2)
-                -- no_heal_equips_check()
+                ReserveScript("vakarine_equip_equips_check()", 0.2)
+                -- vakarine_equip_equips_check()
                 return
             end
         end
@@ -370,10 +349,10 @@ function no_heal_animus_unequip(frame, msg, str, num)
     else
         DO_WEAPON_SWAP(frame, 1)
     end
-    no_heal_item_equip()
+    vakarine_equip_item_equip()
 end
 
-function no_heal_equips_check()
+function vakarine_equip_equips_check()
     local equipItemList = session.GetEquipItemList();
     local cnt = equipItemList:Count();
 
@@ -398,7 +377,7 @@ function no_heal_equips_check()
     imcAddOn.BroadMsg("NOTICE_Dm_stage_start", "[NH]End of Operation", 2);
 end
 
-function no_heal_stat_update()
+function vakarine_equip_stat_update()
 
     local frame = ui.GetFrame("charbaseinfo1_my")
     if (not frame) then
@@ -438,26 +417,26 @@ function no_heal_stat_update()
     hptext:SetOffset(hp:GetX(), hp:GetY() - 10 - (15 - 15))
 end
 
-function NO_HEAL_ON_INIT(addon, frame)
+function VAKARINE_EQUIP_ON_INIT(addon, frame)
 
     g.addon = addon
     g.frame = frame
     g.cid = info.GetCID(session.GetMyHandle())
     g.lang = option.GetCurrentCountry()
-    no_heal_load_settings()
+    vakarine_equip_load_settings()
 
-    addon:RegisterMsg('BUFF_ADD', 'no_heal_BUFF_ON_MSG')
-    addon:RegisterMsg('BUFF_UPDATE', 'no_heal_BUFF_ON_MSG');
-    addon:RegisterMsg('STAT_UPDATE', 'no_heal_stat_update');
-    addon:RegisterMsg('TAKE_DAMAGE', 'no_heal_stat_update');
-    addon:RegisterMsg('TAKE_HEAL', 'no_heal_stat_update');
+    addon:RegisterMsg('BUFF_ADD', 'vakarine_equip_BUFF_ON_MSG')
+    addon:RegisterMsg('BUFF_UPDATE', 'vakarine_equip_BUFF_ON_MSG');
+    addon:RegisterMsg('STAT_UPDATE', 'vakarine_equip_stat_update');
+    addon:RegisterMsg('TAKE_DAMAGE', 'vakarine_equip_stat_update');
+    addon:RegisterMsg('TAKE_HEAL', 'vakarine_equip_stat_update');
 
-    addon:RegisterMsg("GAME_START", "no_heal_switching")
+    addon:RegisterMsg("GAME_START", "vakarine_equip_switching")
     if g.settings[g.cid].use == 0 then
         return
     end
 
-    if not no_heal_vakarine() then
+    if not vakarine_equip_vakarine() then
         return
     end
 
@@ -465,6 +444,7 @@ function NO_HEAL_ON_INIT(addon, frame)
     local curMap = GetZoneName(pc)
     local mapCls = GetClass("Map", curMap)
     -- print(tostring(mapCls.MapType))
+    g.equip_iesids = g.equip_iesids or {}
     if mapCls.MapType == "Instance" then
         local unequip_tbl = {30 -- RH_SUB
         , 31 -- LH_SUB
@@ -480,7 +460,16 @@ function NO_HEAL_ON_INIT(addon, frame)
         , 34 -- SHOULDER
         , 33 -- BELT
         }
-        -- g.settings[tostring(g.cid)]["equip_iesid"] = {}
+
+        for i, equip in ipairs(g.util_tbl) do
+            for equip_name, equip_info in pairs(equip) do
+                for equip_index, check_str in pairs(equip_info) do
+                    print(string.format("装備名: %s, 装備ID: %d, チェック文字列: %s", equip_name,
+                                        equip_index, check_str))
+                end
+            end
+        end
+
         session.ResetItemList();
         local equipItemList = session.GetEquipItemList();
         for _, spot_index in ipairs(unequip_tbl) do
@@ -493,12 +482,12 @@ function NO_HEAL_ON_INIT(addon, frame)
                 end
             end
         end
-        no_heal_save_settings()
-        addon:RegisterMsg("GAME_START", "no_heal_animus_unequip")
+        vakarine_equip_save_settings()
+        addon:RegisterMsg("GAME_START", "vakarine_equip_animus_unequip")
 
     elseif mapCls.MapType == "City" then
 
-        function no_heal_neck_equip(frame, msg, str, num)
+        function vakarine_equip_neck_equip(frame, msg, str, num)
             local equipItemList = session.GetEquipItemList();
             local equipItem = equipItemList:GetEquipItemByIndex(19);
             local iesid = equipItem:GetIESID()
@@ -508,12 +497,11 @@ function NO_HEAL_ON_INIT(addon, frame)
                 item.Equip(item_index)
             end
         end
-        addon:RegisterMsg("GAME_START", "no_heal_neck_equip")
+        addon:RegisterMsg("GAME_START", "vakarine_equip_neck_equip")
     end
-
 end
 
---[[function no_heal_animus_equip()
+--[[function vakarine_equip_animus_equip()
 
     for spot, iesid in pairs(g.settings[tostring(g.cid)]["equip_iesid"]) do
         local equip_item = session.GetInvItemByGuid(iesid);
