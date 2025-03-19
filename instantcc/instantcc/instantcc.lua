cc1eaf270358b7d8e68283a437acb5a543db9cb3
norisan <128560971+ajinorisan@users.noreply.github.com>
2025-03-19 09:47:59 +0900

    a

--- instantcc/instantcc/instantcc.lua
+++ instantcc/instantcc/instantcc.lua
@@ -5,11 +5,12 @@
 -- v1.0.4 他に干渉しない様に
 -- v1.0.5 速攻バグ修正
 -- v1.0.6 設定ジョブ取れる様に。
+-- v1.0.7 コンテキスト表示切替
 local addonName = "INSTANTCC"
 local addonNameLower = string.lower(addonName)
 
 local author = "ebisuke"
-local ver = "1.0.6"
+local ver = "1.0.7"
 local basever = "0.0.7"
 
 _G["ADDONS"] = _G["ADDONS"] or {}
@@ -60,15 +61,18 @@ function INSTANTCC_LOAD_SETTINGS()
 
     if not settings then
         settings = {
-            characters = {}
+            characters = {},
+            per_barracks = false
         }
     elseif not settings.characters then
         settings.characters = {}
+    elseif not settings.per_barracks then
+        settings.per_barracks = false
     end
 
     g.settings = settings
 
-    INSTANTCC_SAVE_SETTINGS() -- 必要に応じてこれを呼び出す
+    INSTANTCC_SAVE_SETTINGS()
 end
 
 function INSTANTCC_SORT_CHAR_DATA()
@@ -103,7 +107,6 @@ function INSTANTCC_SORT_CHAR_DATA()
         local found = false
         for j, character in ipairs(g.settings.characters) do
             if character.cid == pc_cid then
-                -- 既存のキャラクター情報を更新
                 g.settings.characters[j] = info
                 found = true
                 break
@@ -134,27 +137,73 @@ function INSTANTCC_ON_INIT(addon, frame)
     g.addon = addon
     g.frame = frame
 
+    g.lang = option.GetCurrentCountry()
+
     INSTANTCC_LOAD_SETTINGS()
 
     g.retry = nil
     g.do_cc = nil
-    g.layer = g.layer or (g.settings and g.settings.charactors and g.settings.charactors.layer) or 1
+    g.layer = g.layer or (g.settings and g.settings.characters and g.settings.characters.layer) or 1
 
     g.SetupHook(INSTANTCC_BARRACK_START_FRAME_OPEN, "BARRACK_START_FRAME_OPEN")
+    -- acutil.setupEvent(addon, "BARRACK_START_FRAME_OPEN", "INSTANTCC_BARRACK_START_FRAME_OPEN");
     g.SetupHook(INSTANTCC_APPS_TRY_MOVE_BARRACK, "APPS_TRY_MOVE_BARRACK")
     g.SetupHook(INSTANTCC_BARRACK_TO_GAME, "BARRACK_TO_GAME")
     INSTANTCC_SORT_CHAR_DATA()
+
+    local apps = ui.GetFrame("apps")
+    local go_barrackmode = GET_CHILD_RECURSIVELY(apps, "GO_BARRACKMODE")
+    AUTO_CAST(go_barrackmode)
+    go_barrackmode:SetEventScript(ui.RBUTTONUP, "INSTANTCC_DISPLAY_TOGGLE")
+    go_barrackmode:SetEventScript(ui.MOUSEON, "INSTANTCC_TEXT_TOOLTIP")
+    go_barrackmode:SetEventScriptArgString(ui.MOUSEON, "ON")
+    go_barrackmode:SetEventScript(ui.MOUSEOFF, "INSTANTCC_TEXT_TOOLTIP")
+    go_barrackmode:SetEventScriptArgString(ui.MOUSEOFF, "OFF")
+    g.go_x = apps:GetX()
+    g.go_y = apps:GetY()
+end
+
+function INSTANTCC_TEXT_TOOLTIP(frame, ctrl, str, num)
+    local tooltip_frame = ui.CreateNewFrame("notice_on_pc", "tooltip_frame", 0, 0, 0, 0)
+    AUTO_CAST(tooltip_frame)
+    tooltip_frame:SetSkinName('bg')
+
+    local notice = tooltip_frame:CreateOrGetControl("richtext", "notice", 5, 5)
+    local text = g.lang == "Japanese" and "{ol}Instant CC{nl}右クリック: 表示方法を切替えます" or
+                     "{ol}Instant CC{nl}Right click: Switch display"
+    notice:SetText(text)
+    tooltip_frame:Resize(notice:GetWidth() + 10, notice:GetHeight() + 10)
+    tooltip_frame:SetPos(g.go_x - notice:GetWidth() - 10, g.go_y + 250)
+
+    if str == "ON" then
+        tooltip_frame:ShowWindow(1)
+    else
+        tooltip_frame:ShowWindow(0)
+    end
+
+end
+
+function INSTANTCC_DISPLAY_TOGGLE(frame, ctrl, str, num)
+    if not g.settings.per_barracks then
+        g.settings.per_barracks = true
+        ui.SysMsg(g.lang == "Japanese" and "バラック毎表示に切替えました" or
+                      "Switched to per-barrack display")
+    else
+        g.settings.per_barracks = false
+        ui.SysMsg(g.lang == "Japanese" and "一覧表示に切替えました" or "Switched to all list display")
+    end
+    INSTANTCC_SAVE_SETTINGS()
 end
 
 function INSTANTCC_APPS_TRY_MOVE_BARRACK()
-    INSTANTCC_APPS_TRY_MOVE_BARRACK_()
+    INSTANTCC_APPS_TRY_MOVE_BARRACK_(1)
 end
 
-function INSTANTCC_APPS_TRY_MOVE_BARRACK_()
+function INSTANTCC_APPS_TRY_MOVE_BARRACK_(barrack_num)
 
     function INSTANTCC_DO_CC(cid, layer)
 
-        if cid ~= nil then
+        if cid then
             g.do_cc = {
                 cid = cid,
                 layer = layer
@@ -168,15 +217,38 @@ function INSTANTCC_APPS_TRY_MOVE_BARRACK_()
     local context = ui.CreateContextMenu("INSTANTCC_SELECT_CHARACTOR", "Barrack Charactor List", 0, 0, 0, 0)
     ui.AddContextMenuItem(context, "Return To Barrack", "INSTANTCC_DO_CC()")
 
-    for i = 1, #g.settings.characters do
-        local info = g.settings.characters[i]
-        local pc_name = info.name
-        local job_cls = GetClassByType("Job", info.jobid)
-        local job_name = GET_JOB_NAME(job_cls, info.gender)
-        local str = "Lv" .. info.level .. " " .. pc_name .. " (" .. job_name .. ")"
-        ui.AddContextMenuItem(context, str, string.format("INSTANTCC_DO_CC('%s',%d)", info.cid, info.layer))
-    end
+    if not g.settings.per_barracks then
 
+        for i = 1, #g.settings.characters do
+            local info = g.settings.characters[i]
+            local pc_name = info.name
+            local job_cls = GetClassByType("Job", info.jobid)
+            local job_name = GET_JOB_NAME(job_cls, info.gender)
+            local str = "Lv" .. info.level .. " " .. pc_name .. " (" .. job_name .. ")"
+            ui.AddContextMenuItem(context, str, string.format("INSTANTCC_DO_CC('%s',%d)", info.cid, info.layer))
+        end
+
+    else
+
+        ui.AddContextMenuItem(context, "Barrack 1", string.format("INSTANTCC_APPS_TRY_MOVE_BARRACK_(%d)", 1))
+        ui.AddContextMenuItem(context, "Barrack 2", string.format("INSTANTCC_APPS_TRY_MOVE_BARRACK_(%d)", 2))
+        ui.AddContextMenuItem(context, "Barrack 3", string.format("INSTANTCC_APPS_TRY_MOVE_BARRACK_(%d)", 3))
+        ui.AddContextMenuItem(context, "----------", "None")
+
+        for i = 1, #g.settings.characters do
+
+            local info = g.settings.characters[i]
+            local layer = info.layer
+            if barrack_num == layer then
+                local pc_name = info.name
+                local job_cls = GetClassByType("Job", info.jobid)
+                local job_name = GET_JOB_NAME(job_cls, info.gender)
+                local str = "Lv" .. info.level .. " " .. pc_name .. " (" .. job_name .. ")"
+                ui.AddContextMenuItem(context, str, string.format("INSTANTCC_DO_CC('%s',%d)", info.cid, info.layer))
+            end
+        end
+
+    end
     ui.OpenContextMenu(context)
 end
 
@@ -189,21 +261,19 @@ function INSTANTCC_BARRACK_START_FRAME_OPEN_(frame)
     if frame == nil then
         return
     end
-
     local bc_frame = ui.GetFrame("barrack_charlist")
     g.layer = tonumber(bc_frame:GetUserValue("SelectBarrackLayer"))
-    local hidelogin = GET_CHILD_RECURSIVELY(frame, "hidelogin", "ui::CCheckBox");
+
+    local hidelogin = GET_CHILD_RECURSIVELY(frame, "hidelogin");
     AUTO_CAST(hidelogin)
     barrack.SetHideLogin(1);
     hidelogin:SetCheck(1);
-    --[[local start_game = GET_CHILD_RECURSIVELY(frame, "start_game")
-    AUTO_CAST(start_game)
-    start_game:SetEventScript(ui.LBUTTONUP, "INSTANTCC_BARRACK_TO_GAME")]]
 
     if g.do_cc and not g.retry then
         g.retry = 0
         ReserveScript("INSTANTCC_CHANGE()", 0.1)
     end
+
 end
 
 function INSTANTCC_CHANGE()
@@ -260,7 +330,7 @@ function INSTANTCC_TOGAME()
         return
     end
 
-    INSTANTCC_BARRACK_TO_GAME()
+    INSTANTCC_BARRACK_TO_GAME_()
 
 end
 
